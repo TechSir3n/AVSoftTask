ed9b35caad290f2ce5c63ed13ce023fb63df06c5
Ruslan <ruslan56781@mail.ru>
2023-11-16 22:14:15 +0300

    add commit

--- diary.cpp
+++ diary.cpp
@@ -8,6 +8,8 @@
 #include <thread>
 #include <atomic>
 #include <algorithm>
+#include <iomanip>
+#include <queue>
 
 using namespace std::chrono;
 
@@ -18,214 +20,236 @@ struct Person {
 };
 
 struct Event {
-  std::chrono::system_clock::time_point created; // time create
-  std::chrono::system_clock::time_point expires; // time finish
+  std::chrono::system_clock::time_point created; 
+  std::chrono::system_clock::time_point expires;
   std::string description;
 };
 
+struct EventComparator {
+  bool operator() (const Event& e1, const Event& e2) const {
+    return e1.expires < e2.expires;
+  }
+};
+
 struct Birthday {
-  std::chrono::system_clock::time_point endDate; // date finish
-  Person personInfo;                             // info about person
+  std::chrono::system_clock::time_point date; 
+  Person full_name; 
   int age;
 
   bool operator<(const Birthday &other) const {
-    // This example compares just by endDate, but you could use any criteria you want
-    return endDate < other.endDate;
+    return date < other.date;
   }
 };
 
-class Diary final {
+class Diary {
 private:
-  std::set<Birthday> st;
-  std::vector<Event> events;
-  std::atomic<bool> exit_flag{false};
-  std::thread *age_updater_thread{nullptr};
-  std::mutex mtx; // to ensure thread safety on events operations
+  std::multiset<Event, EventComparator> events;
+  std::set<Birthday> birthdays;
+  std::thread event_worker;
+  std::atomic<bool> working;
+  std::mutex mtx_events;
+  std::mutex mtx_birthdays;
+
+ void EventWorker() {
+  while (working) {
+    std::this_thread::sleep_for(std::chrono::seconds(60));
+    {
+      std::lock_guard<std::mutex> lg(mtx_events);
+      auto now = std::chrono::system_clock::now();
+      for(auto it = events.begin(); it != events.end(); ){
+        if(now >= it->expires)
+          it = events.erase(it);
+        else
+          ++it;
+      }
+    }
+  }
+ }
 
 public:
-  Diary() {
-    // using detach as requested but consider managing it via shared_ptr or similar structure
-    age_updater_thread = new std::thread([&]
-                                         { this->age_updater(); });
-    age_updater_thread->detach();
-  }
+  Diary() : working(true), event_worker(&Diary::EventWorker, this) {}
 
+  /**
+  * The destructor stops the worker thread.
+  */
   ~Diary() {
-    stopUpdatingAges();
-    if (age_updater_thread->joinable()) {
-      age_updater_thread->join();
-    }
-    delete age_updater_thread;
+    working = false;
+    event_worker.join();
   }
 
-  void addEvent(const std::chrono::system_clock::time_point &expiryDate, const std::string &description) {
-    Event newEvent;
-
-    newEvent.created = std::chrono::system_clock::now();
-    newEvent.expires = expiryDate;
-    newEvent.description = description;
-
-    // mutex for synchronous access to events
-    std::lock_guard<std::mutex> lock(mtx);
-    events.push_back(newEvent);
-  }
-
-  bool addBirthday() {
-    Birthday newBirthday;
-
-    std::cout << "Enter Name then press ENTER: ";
-    std::cin >> newBirthday.personInfo.name;
-
-    std::cout << "Enter Surname then press ENTER: ";
-    std::cin >> newBirthday.personInfo.surname;
-
-    std::cout << "Enter Fatherland then press ENTER: ";
-    std::cin >> newBirthday.personInfo.fatherland;
-
-    std::cout << "Enter Birthday Year (YYYY) then press ENTER: ";
-    int year;
-    std::cin >> year;
-
-    std::cout << "Enter Birthday Month (MM) then press ENTER: ";
-    int month;
-    std::cin >> month;
-
-    std::cout << "Enter Birthday Day (DD) then press ENTER: ";
-    int day;
-    std::cin >> day;
-
-    // convert user input to time_point
-    std::tm birthDay = {};
-    birthDay.tm_year = year - 1900; // years since 1900
-    birthDay.tm_mon = month - 1;    // months since January (0-11)
-    birthDay.tm_mday = day;         // day of the month (1-31)
-
-    newBirthday.endDate = std::chrono::system_clock::from_time_t(std::mktime(&birthDay));
-
-    // Calculating age
-    auto now = std::chrono::system_clock::now();
-    newBirthday.age = std::chrono::duration_cast<std::chrono::hours>(newBirthday.endDate - now).count() / 8760;
-
-    // mutex is not necessary here, as events within single thread (main)
-    if (st.count(newBirthday) > 0) {
-      return false;
-    }
-
-    st.insert(newBirthday);
+  /**
+  * Adds an event to the diary.
+  * @param expires Expiration time of the event.
+  * @param description Description of the event.
+  * @return Whether the event was successfully added.
+  */
+  bool AddEvent(const std::chrono::system_clock::time_point& expires, const std::string& description) {
+    Event e;
+    e.created = std::chrono::system_clock::now();
+    e.expires = expires;
+    e.description = description;
+    std::lock_guard<std::mutex> lg(mtx_events);
+    events.emplace(e);
     return true;
   }
 
-  bool saveFileEvents(const Event &event) {
-    std::ofstream file("output.txt", std::ios::app);
+  /**
+  * Adds a birthday to the diary.
+  * @param date Date of birth.
+  * @param full_name Full name of the person.
+  * @param age The person's current age.
+  * @return Whether the birthday was successfully added.
+  */
+  bool AddBirthday(const std::chrono::system_clock::time_point& date, const Person& full_name, int age) {
+    if(full_name.name.empty() || full_name.surname.empty() || full_name.fatherland.empty() || age < 0) {
+      return false;
+    }
+    
+    Birthday b;
+    b.date = date;
+    b.full_name = full_name;
+    b.age = age;
 
-    if (!file.is_open()) {
-      std::cerr << "Failed to open file::[saveFileEvents]";
+    std::lock_guard<std::mutex> lg(mtx_birthdays);
+    return birthdays.insert(b).second; // вернет false, если такой же Birthday уже существует
+  }
+
+   std::multiset<Event, EventComparator> GetEvents() {
+    std::lock_guard<std::mutex> lg(mtx_events);
+    return events;
+  }
+
+  std::set<Birthday> GetBirthdays() {
+    std::lock_guard<std::mutex> lg(mtx_birthdays);
+    return birthdays;
+  }
+  
+  bool SaveToFile(const std::string& filename) {
+    std::ofstream file(filename);
+    if (!file) {
       return false;
     }
 
-    time_t created_time_t = std::chrono::system_clock::to_time_t(event.created);
-    time_t expired_time_t = std::chrono::system_clock::to_time_t(event.expires);
+    // Function to convert time_point to string
+    auto TimePointToTimeT = [](std::chrono::system_clock::time_point tp) {
+        return std::chrono::system_clock::to_time_t(tp);
+    };
 
-    file << "Описание: " << event.description << std::endl;
-    file << "Дата создания: " << std::ctime(&created_time_t);
-    file << "Дата окончания: " << std::ctime(&expired_time_t);
+    auto TimePointToString = [&TimePointToTimeT](std::chrono::system_clock::time_point tp) {
+        auto tt = TimePointToTimeT(tp);
+        auto tm = *std::localtime(&tt);
+        std::stringstream ss;
+        ss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+        return ss.str();
+    };
+    
+    // Save all events.
+    for (const auto& e : GetEvents()) {
+      file << "Event created at " << TimePointToString(e.created) 
+           << ", expires at " << TimePointToString(e.expires)
+           << ", description: " << e.description << '\n';
+    }
+
+    // Save all birthdays.
+    for (const auto& b : GetBirthdays()) {
+      file << "Birthday of " << b.full_name.name << " " << b.full_name.surname << " " << b.full_name.fatherland
+           << ", born at " << TimePointToString(b.date)
+           << ", age " << b.age << '\n';
+    }
 
     return true;
-  }
+ }
 
-  void saveAllEventsToFile() {
-    for (const auto &event : events) {
-      saveFileEvents(event);
-    }
-  }
+  /**
+  * Runs the interactive mode allowing the user to interact with the diary.
+ */
+ void runInteractiveMode() {
+    int choice = 0;
+    
+    // Keep running the interactive mode until the user selects the exit option
+    while (choice != 3) {
+        std::cout << "Select an option:\n"
+                  << "1. Add Event\n"
+                  << "2. Add Birthday\n"
+                  << "3. Exit\n";
+        std::cin >> choice;
 
-  void age_updater() {
-    while (!exit_flag.load()) {
-      auto now = std::chrono::system_clock::now();
-      std::set<Birthday> updatedBirthdays;
-      {
-        std::lock_guard<std::mutex> lock(mtx);
-        for (const auto &birthday : st) {
-          Birthday updatedBirthday = birthday;
-          if (now >= birthday.endDate) {
-            updatedBirthday.age += 1;
-            updatedBirthday.endDate += std::chrono::hours(8760); // Assuming the birthday always happens after 1 year
-          }
-          updatedBirthdays.insert(updatedBirthday);
+        if (choice == 1) {
+            // Enter event details
+            std::string description;
+            std::time_t expires;
+
+            std::cout << "Enter event description: ";
+            std::cin.ignore();
+            std::getline(std::cin, description);
+                    
+            std::cout << "Enter event expiry time (in seconds from now): ";
+            std::cin >> expires;
+                    
+            auto expires_timepoint = std::chrono::system_clock::now() + std::chrono::seconds(expires);
+            this->AddEvent(expires_timepoint, description);
+        } else if (choice == 2) {
+            // Enter person details
+            std::string name, surname, fatherland;
+            std::time_t birth_date;
+
+            std::cout << "Enter person's name: ";
+            std::cin >> name;
+
+            std::cout << "Enter person's surname: ";
+            std::cin >> surname;
+
+            std::cout << "Enter person's fatherland: ";
+            std::cin >> fatherland;
+
+            std::cout << "Enter person's birth date (in seconds from now): ";
+            std::cin >> birth_date;
+
+            Person p;
+            p.name = name;
+            p.surname = surname;
+            p.fatherland = fatherland;
+
+            auto birth_date_timepoint = std::chrono::system_clock::now() + std::chrono::seconds(birth_date); // if the birthday is in the future
+            this->AddBirthday(birth_date_timepoint, p, 0);  // age is set to 0 assuming the person is not born yet
         }
-        std::swap(st, updatedBirthdays);
-      }
-      std::this_thread::sleep_for(std::chrono::hours(24));
     }
+}
+
+
+  void Print() {
+  auto TimePointToTimeT = [](std::chrono::system_clock::time_point tp) {
+    return std::chrono::system_clock::to_time_t(tp);
+  };
+
+  auto TimePointToString = [&TimePointToTimeT](std::chrono::system_clock::time_point tp) {
+    auto tt = TimePointToTimeT(tp);
+    auto tm = *std::localtime(&tt);
+    std::stringstream ss;
+    ss << std::put_time(&tm, "%Y-%m-%d %H:%M:%S");
+    return ss.str();
+  };
+
+
+  for (const auto& e : GetEvents()) {
+    std::cout << "Event created at " << TimePointToString(e.created)
+      << ", expires at " << TimePointToString(e.expires)
+      << ", description: " << e.description << '\n';
   }
 
-  void removeExpiredEvents() {
-
-    auto now = std::chrono::system_clock::now();
-
-    // Remove all expired events
-    std::lock_guard<std::mutex> lock(mtx);
-    events.erase(
-        std::remove_if(events.begin(), events.end(),
-                       [&](const Event &event)
-                       { return event.expires < now; }),
-        events.end());
+  for (const auto& b : GetBirthdays()) {
+    std::cout << "Birthday of " << b.full_name.name << " " << b.full_name.surname << " " << b.full_name.fatherland
+      << ", born at " << TimePointToString(b.date)
+      << ", age " << b.age << '\n';
   }
+}
 
-  void outputEvent(const Event &event) {
-    time_t created_time_t = std::chrono::system_clock::to_time_t(event.created);
-    time_t expired_time_t = std::chrono::system_clock::to_time_t(event.expires);
-
-    std::cout << "Описание: " << event.description << std::endl;
-    std::cout << "Дата создания: " << std::ctime(&created_time_t);
-    std::cout << "Дата окончания: " << std::ctime(&expired_time_t);
-  }
-
-  void outputEventToday() {
-    auto now = std::chrono::system_clock::now();
-    time_t tt = std::chrono::system_clock::to_time_t(now);
-    auto today = std::chrono::system_clock::from_time_t(86400 * (tt / 86400));
-
-    std::vector<Event> eventsToday;
-   {
-      std::lock_guard<std::mutex> lock(mtx);
-      std::copy_if(events.begin(), events.end(),
-                   std::back_inserter(eventsToday),
-                   [&](const Event &event)
-                   { return event.created >= today && event.created < today + std::chrono::hours(24); });
-    }
-
-    for (auto &event : eventsToday) {
-      outputEvent(event);
-    }
-  }
-
-  void stopUpdatingAges() {
-    exit_flag = true;
-  }
 };
 
-int main()
-{
-  Diary myDiary;
+int main() {
+    Diary my_diary;
+    my_diary.Print();
 
-  auto eventExpiryDate = std::chrono::system_clock::now() + std::chrono::hours(48); // event expires in two days
-  std::string eventDescription = "Meeting with friends.";
-
-  myDiary.addEvent(eventExpiryDate, eventDescription);
-
-  Birthday bd;
-  myDiary.addBirthday();
-
-  myDiary.outputEventToday();
-  myDiary.stopUpdatingAges();
-
-  Event ev;
-  ev.created = std::chrono::system_clock::now();
-  ev.expires = eventExpiryDate;
-  ev.description = eventDescription;
-
-  myDiary.saveFileEvents(ev);
-
-  return 0;
+    return 0;
 }
+
